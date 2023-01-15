22b6af91d4c6561494480a07894e79d17a7fd300
rafaski1 <rav.skibinski@gmail.com>
2023-01-15 14:51:25 +0100

    sql ops docstrings

--- ecommerce_api/sql/operations.py
+++ ecommerce_api/sql/operations.py
@@ -16,12 +16,14 @@ class UserOperations:
 
     @database_operation
     def create(self, db: Session, user: User) -> None:
+        """Create new user"""
         user.password = to_hash(user.password)
         db.add(**user.dict())
         db.commit()
 
     @database_operation
     def get_by_email(self, db: Session, email: str) -> Optional[models.User]:
+        """Get user by email"""
         user = db.query(models.User).filter(
             models.User.email == email
         ).first()
@@ -29,11 +31,13 @@ class UserOperations:
 
     @database_operation
     def get_all(self, db: Session) -> Optional[List[models.User]]:
+        """Get all users from db"""
         all_users = db.query(models.User).all()
         return all_users
 
     @database_operation
     def delete(self, db: Session, email: str) -> None:
+        """Delete a user"""
         db.query(models.User).filter(models.User.email == email).delete()
         db.commit()
 
@@ -45,6 +49,7 @@ class ProductOperations:
 
     @database_operation
     def create(self, db: Session, product: Product) -> None:
+        """Create a product"""
         db.add(**product.dict())
         db.commit()
 
@@ -59,6 +64,7 @@ class ProductOperations:
         db: Session,
         product_id: str
     ) -> Optional[models.Product]:
+        """Get product by id"""
         product = db.query(models.Product).filter(
             models.Product.id == product_id
         ).first()
@@ -70,6 +76,7 @@ class ProductOperations:
         db: Session,
         category: str
     ) -> Optional[List[models.Product]]:
+        """Get product by category"""
         matching_products = db.query(models.Product).filter(
             models.Product.category == category
         ).all()
@@ -82,6 +89,7 @@ class ProductOperations:
         product_id: str,
         product: Product
     ) -> models.Product:
+        """Update product"""
         updated_product = db.query(models.Product).filter(
             models.Product.id == product_id
         ).update(**product.dict())
@@ -90,6 +98,7 @@ class ProductOperations:
 
     @database_operation
     def remove(self, db: Session, product_id: str) -> None:
+        """Delete a product from db"""
         db.query(models.Product).filter(
             models.Product.id == product_id
         ).delete()
@@ -109,6 +118,7 @@ class OrderOperations:
         order_id: str,
         product_id: str
     ) -> None:
+        """Add product to cart"""
         product = db.query(models.Product).get(product_id).first()
         product.update({"quantity": product.quantity - 1})
 
@@ -128,6 +138,7 @@ class OrderOperations:
 
     @database_operation
     def get(self, db: Session, order_id: str = None) -> models.Order:
+        """Get order (all items in cart)"""
         order = db.query(models.Order).filter(
             models.Order.id == order_id
         ).all()
@@ -135,6 +146,7 @@ class OrderOperations:
 
     @database_operation
     def get_all(self, db: Session, email: str = None) -> List[models.Order]:
+        """Get all orders from db"""
         if not email:
             return db.query(models.Order).all()
         orders = db.query(models.Order).filter(
@@ -144,6 +156,7 @@ class OrderOperations:
 
     @database_operation
     def remove(self, db: Session, order_id: str, product_id: str) -> None:
+        """Remove an item from order (cart)"""
         order = db.query(models.Order).filter(
             models.Order.id == order_id
         ).first()
@@ -156,6 +169,7 @@ class OrderOperations:
 
     @database_operation
     def submit(self, db: Session, order_id: str) -> models.Order:
+        """Submit an order (finalize cart)"""
         order = db.query(models.Order).filter(
             models.Order.id == order_id
         ).first()
